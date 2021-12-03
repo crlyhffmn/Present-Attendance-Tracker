605a1a5be1cd6f60f0c8c2e3bf5e2b614dc575a1
crlyehffmn <crlyehffmn@gmail.com>
2021-12-03 10:30:06 -0500

    basic routing

--- project-2/src/App.tsx
+++ project-2/src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path='/home' element={<HomePage />} />
+          {/* <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} /> */}
+        </Routes>
+      </div>
+
+    </BrowserRouter>
   );
 }
 
