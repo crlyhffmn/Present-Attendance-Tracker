d2646c9f45a0ec191f69e10066177482386cfc14
crlyehffmn <crlyehffmn@gmail.com>
2021-12-03 11:19:15 -0500

    updating Navbar

--- project-2/src/components/Navbar/index.tsx
+++ project-2/src/components/Navbar/index.tsx
@@ -9,13 +9,19 @@ const Navbar = () => {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            <a className="nav-link" href="/home">Home</a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="#">About</a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="#">Contact</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Register</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Log In</a>
           </li>
         </ul>
       </div>
