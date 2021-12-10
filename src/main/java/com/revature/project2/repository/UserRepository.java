abf8b6c62550de8a5954823ba8863911d0ebc6c4
crlyehffmn <crlyehffmn@gmail.com>
2021-12-10 15:42:34 -0500

    Backend Setup Things

--- src/main/java/com/revature/project2/repository/UserRepository.java
+++ src/main/java/com/revature/project2/repository/UserRepository.java
@@ -2,8 +2,11 @@ package com.revature.project2.repository;
 
 import com.revature.project2.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+//    @Query("select u from User")
+//    User findUserByUsername(String username);
 }
